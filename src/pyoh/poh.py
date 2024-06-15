13485c62bf00b9d8a4310bb0fbbf4bf7bd58531b
Eva Pace <eba.pachi@gmail.com>
2024-06-15 17:41:05 -0300

    add prints to check reset is working

--- src/pyoh/poh.py
+++ src/pyoh/poh.py
@@ -8,6 +8,7 @@ class Poh:
         self.remaining_hashes = hashes_per_tick
 
     def hash_next(self, data: str):
+        print("new hash")
         self.curr_hash = sha256((self.curr_hash + data).encode()).hexdigest()
         self.num_hashes += 1
         self.remaining_hashes -= 1
@@ -16,6 +17,7 @@ class Poh:
         while self.remaining_hashes:
             self.hash_next(mixin)
         final = self.curr_hash
+        print("reset")
         self.reset()
         return final
 
